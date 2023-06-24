3db73c44db5410d91d16a2500f739ab1d7b62c59
Huseyin Koclar <mhuseyin.koclar@gmail.com>
2023-06-24 18:45:23 +0300

    Add files via upload

--- bookscrape/pipelines.py
+++ bookscrape/pipelines.py
@@ -5,9 +5,20 @@
 
 
 # useful for handling different item types with a single interface
+import pymongo
 from itemadapter import ItemAdapter
 
 
 class BookscrapePipeline:
+
+    def __init__(self):
+        self.conn = pymongo.MongoClient(
+            'localhost',
+            27017
+        )
+        db = self.conn['smartmaple']
+        self.collection = db['kitapyurdu']
+
     def process_item(self, item, spider):
+        self.collection.insert(dict(item))
         return item
